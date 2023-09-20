8c7548e1a29b39b4c55dcee9910082d7b0a8536c
Pooja Mane <manepoojad@gmail.com>
2023-09-20 22:46:45 +0530

    toast added success and error

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
-import './App.css';
-import RootRouting from './container/RootRouting';
-import { store } from './redux/store'
-import { Provider } from 'react-redux'
-
+import "./App.css";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
+import RootRouting from "./container/RootRouting";
+import { store } from "./redux/store";
+import { Provider } from "react-redux";
 
 function App() {
-
   return (
     <Provider store={store}>
       <div className="App">
+        <ToastContainer />
         <RootRouting />
       </div>
     </Provider>
   );
 }
-
-
-
-
 
 export default App;
--- src/redux/slice/todoSlice.js
+++ src/redux/slice/todoSlice.js
@@ -1,67 +1,67 @@
-import { createSlice, current } from '@reduxjs/toolkit'
-import { getTodoListAction, createTodoAction, updateTodoAction, deleteTodoAction } from '../thunk/todoThunk'
+import { createSlice, current } from "@reduxjs/toolkit";
+import { toast } from "react-toastify";
+import {
+  getTodoListAction,
+  createTodoAction,
+  updateTodoAction,
+  deleteTodoAction,
+} from "../thunk/todoThunk";
 
 const initialState = {
-    isLoading: false,
-    todoList: [],
-}
+  isLoading: false,
+  todoList: [],
+};
 
 export const todoSlice = createSlice({
-    name: 'todoSlice',
-    initialState,
-    reducers: {
-    },
-    extraReducers: (builder) => {
-        builder
-            .addCase(getTodoListAction.pending, (state, action) => {
-                state.isLoading = true
-            })
-            .addCase(getTodoListAction.fulfilled, (state, action) => {
-                state.isLoading = false
-                state.todoList = action.payload
-            })
-            .addCase(getTodoListAction.rejected, (state, action) => {
-                state.isLoading = false
-            })
+  name: "todoSlice",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(getTodoListAction.pending, (state, action) => {
+        state.isLoading = true;
+      })
+      .addCase(getTodoListAction.fulfilled, (state, action) => {
+        toast.success("todo fetch successfully!");
+        state.isLoading = false;
+        state.todoList = action.payload;
+      })
+      .addCase(getTodoListAction.rejected, (state, action) => {
+        toast.error("something went wrong!");
+        state.isLoading = false;
+      })
 
+      .addCase(createTodoAction.pending, (state, action) => {
+        state.isLoading = true;
+      })
+      .addCase(createTodoAction.fulfilled, (state, action) => {
+        state.isLoading = false;
+      })
+      .addCase(createTodoAction.rejected, (state, action) => {
+        state.isLoading = false;
+      })
 
-            .addCase(createTodoAction.pending, (state, action) => {
-                state.isLoading = true
-            })
-            .addCase(createTodoAction.fulfilled, (state, action) => {
-                state.isLoading = false
-            })
-            .addCase(createTodoAction.rejected, (state, action) => {
-                state.isLoading = false
-            })
+      .addCase(updateTodoAction.pending, (state, action) => {
+        state.isLoading = true;
+      })
+      .addCase(updateTodoAction.fulfilled, (state, action) => {
+        state.isLoading = false;
+      })
+      .addCase(updateTodoAction.rejected, (state, action) => {
+        state.isLoading = false;
+      })
 
-
-            .addCase(updateTodoAction.pending, (state, action) => {
-                state.isLoading = true
-            })
-            .addCase(updateTodoAction.fulfilled, (state, action) => {
-                state.isLoading = false
-            })
-            .addCase(updateTodoAction.rejected, (state, action) => {
-                state.isLoading = false
-            })
-
-
-            .addCase(deleteTodoAction.pending, (state, action) => {
-                state.isLoading = true
-            })
-            .addCase(deleteTodoAction.fulfilled, (state, action) => {
-                state.isLoading = false
-            })
-            .addCase(deleteTodoAction.rejected, (state, action) => {
-                state.isLoading = false
-            })
-    },
-})
-
+      .addCase(deleteTodoAction.pending, (state, action) => {
+        state.isLoading = true;
+      })
+      .addCase(deleteTodoAction.fulfilled, (state, action) => {
+        state.isLoading = false;
+      })
+      .addCase(deleteTodoAction.rejected, (state, action) => {
+        state.isLoading = false;
+      });
+  },
+});
 
 // Action creators are generated for each case reducer function
-export const {
-} = todoSlice.actions
-
-
+export const {} = todoSlice.actions;
